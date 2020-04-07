f27c0a977f161cf02278e2d7728b979025d655be
PascalCoder <pbaconqueror7@yahoo.com>
2020-04-07 14:41:43 -0400

    Completed second module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -10,6 +11,44 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        override public char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var top20Division = (int)(Students.Count * 0.2);
+            var second20Division = (int)(Students.Count * 0.4);
+            var third20Division = (int)(Students.Count * 0.6);
+            var fourth20Division = (int)(Students.Count * 0.8);
+            var sortedStudentsByGrade = Students.OrderByDescending(s => s.AverageGrade).ToList();
+            
+            if (averageGrade >= sortedStudentsByGrade[top20Division].AverageGrade)
+            {
+                return 'A';
+            } 
+            else if (averageGrade < sortedStudentsByGrade[top20Division].AverageGrade
+                && averageGrade >= sortedStudentsByGrade[second20Division].AverageGrade)
+            {
+                return 'B';
+            } 
+            else if (averageGrade < sortedStudentsByGrade[second20Division].AverageGrade
+                && averageGrade >= sortedStudentsByGrade[third20Division].AverageGrade)
+            {
+                return 'C';
+            }
+            else if (averageGrade < sortedStudentsByGrade[third20Division].AverageGrade
+                && averageGrade >= sortedStudentsByGrade[fourth20Division].AverageGrade)
+            {
+                return 'D';
+            }
+            else
+            { }
+
+            return 'F';
         }
     }
 }
